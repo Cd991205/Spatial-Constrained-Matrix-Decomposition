684ce83fdaeb1e1322ab15c9c798ba9040823d35
Chengyu Du <122925086+Cd991205@users.noreply.github.com>
2023-11-04 19:21:31 -0500

    Add files via upload

--- Spatial-Constrained-Matrix-Decomposition/Code/svd.cpp
+++ Spatial-Constrained-Matrix-Decomposition/Code/svd.cpp
@@ -1,60 +1,71 @@
-// svd.cpp
-#include <Rcpp.h>
-using namespace Rcpp;
-#include <vector>
-#include <RcppArmadillo.h>
 // [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::plugins(cpp11)]]
+#include <RcppArmadillo.h>
+#include <Rcpp.h>
 #include <iostream>
-#include <vector>
-#include <Eigen/Dense>
-#include <Eigen/SVD>
-
+using namespace Rcpp;
 
 // [[Rcpp::export]]
-List performSVD(const Eigen::MatrixXd& X) {
-    Eigen::JacobiSVD<Eigen::MatrixXd> svd(X, Eigen::ComputeThinU | Eigen::ComputeThinV);
-    Eigen::MatrixXd U = svd.matrixU();
-    Eigen::MatrixXd V = svd.matrixV();
-
-    // TODO: Further operations based on U, V, and E
-
-    return List::create(Named("U") = U, Named("V") = V);
+Rcpp::List mySVD(const arma::sp_mat& X, int k) {
+  arma::mat U;
+  arma::vec s;
+  arma::mat V;
+  
+  // Calculate the first k singular values using svds
+  arma::svds(U, s, V, X, k);
+  
+  return Rcpp::List::create(Rcpp::Named("U") = U, 
+                            Rcpp::Named("s") = s, 
+                            Rcpp::Named("V") = V);
 }
 
 
-// [[Rcpp::export]]
-double computeR(const Eigen::MatrixXd& X, const Eigen::MatrixXd& V) {
-    Eigen::MatrixXd XV = X * V;
-    double result = 0;
 
-    // Summation based on the given formula
-    for (int i = 0; i < X.rows(); ++i) {
-        for (int j = 0; j < X.rows(); ++j) {
-            result += (XV.row(i) - XV.row(j)).norm();
-        }
+// Calculate R(XV)
+// [[Rcpp::export]]
+double calculate_regularization(const arma::mat& X, const arma::mat& V, const arma::mat& D) {
+  arma::mat XV = X * V;
+  arma::mat reg_term = D * XV; 
+  return arma::accu(reg_term % reg_term);
+}
+
+
+// Gradient Descent Algorithm for Matrix Factorization with Regularization
+// [[Rcpp::export]]
+Rcpp::List gradient_descent_svd(const arma::mat& X, arma::mat U, arma::mat Sigma, arma::mat V, 
+                                const arma::mat& D, double lambda, double alpha, double tol, int max_iter) {
+  arma::mat I_U = arma::eye<arma::mat>(U.n_rows, U.n_rows);
+  arma::mat I_V = arma::eye<arma::mat>(V.n_cols, V.n_cols);
+  double f = arma::norm(X - U * Sigma * V.t(), "fro") + lambda * calculate_regularization(X, V, D);
+  
+  for (int iter = 0; iter < max_iter; ++iter) {
+    // Compute the gradients based on provided derivatives
+    arma::mat grad_U = -2 * Sigma * V.t() * X.t() + 2 * Sigma * V.t() * V * Sigma.t() * U.t();
+    arma::mat grad_V = -2 * Sigma.t() * U.t() * X + 2 * Sigma.t() * U.t() * U * Sigma * V.t() + 2 * lambda * V.t() * X.t() * D.t() * D * X;
+    
+    // Transpose gradients to match dimensions of U and V
+    grad_U = grad_U.t();
+    grad_V = grad_V.t();
+    
+    // Update the matrices U and V
+    U -= alpha * grad_U;
+    V -= alpha * grad_V;
+    
+    // Compute the new objective function value
+    double f_new = arma::norm(X - U * Sigma * V.t(), "fro") + lambda * calculate_regularization(X, V, D);
+    
+    // Check for convergence
+    if (std::abs(f_new - f) < tol) {
+      Rcpp::Rcout << "Convergence reached at iteration " << iter << std::endl;
+      break;
     }
-
-    return result;
+    
+    f = f_new; // Update the objective function value
+  }
+  
+  return Rcpp::List::create(Rcpp::Named("U") = U,
+                            Rcpp::Named("Sigma") = Sigma,
+                            Rcpp::Named("V") = V);
 }
-
-// [[Rcpp::export]]
-Eigen::MatrixXd gradientDescent(const Eigen::MatrixXd& X, const Eigen::MatrixXd& U, const Eigen::MatrixXd& E, const Eigen::MatrixXd& D, double lambda, int maxIterations, double learningRate) {
-    int n = X.cols(); // Assuming V has the same number of columns as X
-    Eigen::MatrixXd V = Eigen::MatrixXd::Random(n, n); // Random initialization
-
-    for (int i = 0; i < maxIterations; ++i) {
-        // Compute the gradient of the objective function with respect to V
-        //Eigen::MatrixXd gradientU = 2 * (U * X - V * E.transpose()) * X.transpose();
-        Eigen::MatrixXd gradientV = 2 * (V.transpose() * X.transpose() - U * E) * X + 2 * lambda * V.transpose() * (D * (X * V)).transpose() * (D * (X * V));
-
-        // Update U and V using the computed gradient
-        //U = U - learningRate * gradientU;
-        V = V - learningRate * gradientV.transpose();
-    }
-
-    return V; // Depending on your needs, you can also return U
-}
-
-
 
 
